daa679df9a53d861d543c7496955c72d777e5c56
rocku2008 <shresthasameer703@gmail.com>
2025-04-24 21:42:02 -0400

    adding runner.py file

--- runner.py
+++ runner.py
@@ -7,11 +7,11 @@ sys.dont_write_bytecode = True
 def main():
     # Customize the arguments for pytest run
     pytest_args = [
-        "tests/table2",                      # Folder containing your test cases
+        "tests//table2",                      # Folder containing your test cases
         "-v",                          # Verbose output
         "--tb=short",                  # Short traceback format
         "--capture=tee-sys",          # Capture and also print stdout/stderr
-        "--html=reports/report.html", # HTML report generation
+        "--html=reports//report.html", # HTML report generation
         "--self-contained-html",      # Standalone HTML with styles/scripts
         "-m", "not ignore",           # Run all tests except those marked with @pytest.mark.ignore
     ]
